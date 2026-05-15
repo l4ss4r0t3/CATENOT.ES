0a0c8585a56d0ca5a8ebc1f9dd8fe53329ab8cfa
Caio Ronald Silva <l4ss4r0t3@gmail.com>
2026-05-15 18:37:52 -0300

    add pan system

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,33 @@
+// VARIÁVEIS PARA CONTAINER PRINCIPAL E O WORLD
+const windowContainer = document.getElementById('window');
+const worldContainer = document.getElementById('world');
+
 // VARIÁVEIS PARA O PAN E ZOOM
 let offsetX = 0;
 let offsetY = 0;
 let scale = 1;
+let isPanning = false;
+let mouseX = 0;
+let mouseY = 0;
 
-// VARIÁVEIS PARA CONTAINER PRINCIPAL EO WORLD
-const windowContainer = document.getElementById('window');
-const worldContainer = document.getElementById('world');
+// LISTENER QUE SALVA A CORDENADA EM QUE O MOUSE FOI CLICADO
+windowContainer.addEventListener('mousedown', () => {
+    isPanning = true;
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+// LISTENER QUE ATUALIZA A POSIÇÃO DO WORLD DE ACORDO COM A CORDENADA DO MOUSE
+windowContainer.addEventListener('mousemove', () => {
+    if (isPanning) {
+        offsetX += event.clientX - mouseX;
+        offsetY += event.clientY - mouseY;
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+        worldContainer.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+    }
+});
+// LISTENER PARA INTERROMPER A MOVIMENTAÇÃO DO WORLD
+// (NO OBJETO GLOBAL DO BROWSER PARA EVITAR O "MOUSE PRESO")
+window.addEventListener('mouseup', () => {
+    isPanning = false;
+});
