609e3c8523fdb05ed1a64fcd85ef092566540db3
Caio Ronald Silva <l4ss4r0t3@gmail.com>
2026-05-15 00:37:02 -0300

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+// VARIÁVEIS PARA O PAN E ZOOM
+let offsetX = 0;
+let offsetY = 0;
+let scale = 1;
+
+// VARIÁVEIS PARA CONTAINER PRINCIPAL EO WORLD
+const windowContainer = document.getElementById('window');
+const worldContainer = document.getElementById('world');
